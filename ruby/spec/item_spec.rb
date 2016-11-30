8face6006e27a16a20bab4c335cf112a19c5f147
hanrattyjen <hanratty.jen@gmail.com>
2016-11-30 12:38:39 +0000

    Add passing initialization test for Item class

---
+++ ruby/spec/item_spec.rb
@@ -0,0 +1,11 @@
+require 'item'
+
+describe Item do
+
+  subject(:account) { described_class.new("Item", 1, 5)}
+
+  it 'is initialized with a name, value and quality' do
+    expect(subject.to_s).to eq "Item, 1, 5"
+  end
+
+end
