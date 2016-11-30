9e76e9f1dc2421d9b02ca587534a5e282030cf8f
hanrattyjen <hanratty.jen@gmail.com>
2016-11-30 13:35:57 +0000

    Refactored test to move each mock object within their own context.
    One failing test.

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -5,13 +5,10 @@ require 'gilded_rose'
 describe GildedRose do
 
   subject(:gilded_rose) { described_class.new(item) }
-  let(:item) { Item.new("Item", 3, 6) }
-  let(:low_quality_item) { Item.new("Crap", 2, 1) }
-  let(:old_item) { Item.new("Old", 3, 52) }
-  let(:aged_brie) { Item.new("Aged Brie", 2, 2) }
-  let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 10, 10) }
 
   describe '#update_quality' do
+    let(:item) { Item.new("Item", 3, 6) }
+
     it 'does not change the name' do
       items = [item]
       GildedRose.new(items).update_quality
@@ -29,6 +26,8 @@ describe GildedRose do
     end
 
     context 'The quality of an item is never negative' do
+      let(:low_quality_item) { Item.new("Crap", 2, 1) }
+
       it 'will never have a negative quality' do
         items = [low_quality_item]
         2.times do
@@ -39,6 +38,8 @@ describe GildedRose do
     end
 
     context 'Aged Brie increases in quality the older it gets' do
+      let(:aged_brie) { Item.new("Aged Brie", 2, 2) }
+
       describe 'Aged Brie' do
         it 'increases in quality' do
           items = [aged_brie]
@@ -51,6 +52,8 @@ describe GildedRose do
     end
 
     context 'The quality of an item is never more than 50' do
+      let(:old_item) { Item.new("Old", 3, 52) }
+
       describe 'Older items' do
         it 'cannnot have quality more than 50' do
           items = [old_item]
@@ -60,6 +63,8 @@ describe GildedRose do
     end
 
     context 'Sulfuras never has to be sold or decreases in Quality' do
+      let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 10, 10) }
+
       describe 'Sulfuras' do
         it 'does not lose it\'s Quality' do
           items = [sulfuras]
@@ -78,6 +83,30 @@ describe GildedRose do
           expect(items[0].sell_in).to eq 10
         end
       end
+    end
+
+    context 'Backstage passes increase in quality' do
+      describe 'Backstage passes' do
+        let(:backstage) { Item.new("Backstage passes", 12, 6) }
+
+        it 'increases in value as Sellin value approaches' do
+          items = [backstage]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq 7
+        end
+
+        it 'increases in value by 2 when there are <= 10 Sellin days left' do
+
+        end
+
+        it 'increases in value by 3 when there are <= 10 Sellin dats left' do
+
+        end
+
+        it 'has a quality that drops to zero after the concert' do
+
+        end
+       end
     end
 
   end
