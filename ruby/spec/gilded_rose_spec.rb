3163030e70d1bc0b5465e9007e56a5ab94219f07
hanrattyjen <hanratty.jen@gmail.com>
2016-11-30 13:17:59 +0000

    Test that the quality of an item will never be zero.

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -6,6 +6,8 @@ describe GildedRose do
 
   subject(:gilded_rose) { described_class.new(item) }
   let(:item) { Item.new("Item", 3, 6) }
+  let(:low_quality_item) { Item.new("Crap", 2, 1) }
+
   let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 10, 10) }
 
   describe '#update_quality' do
@@ -41,6 +43,16 @@ describe GildedRose do
           GildedRose.new(items).update_quality # gives quality = 5, days=2
         end
         expect(items[0].quality).to eq 1
+      end
+    end
+
+    context 'The quality of an item is never negative' do
+      it 'will never have a negative quality' do
+        items = [low_quality_item]
+        2.times do
+          GildedRose.new(items).update_quality
+        end
+        expect(items[0].quality).to eq 0
       end
     end
   end
