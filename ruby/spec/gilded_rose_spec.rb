75a2c1e5aa890d9d3981756021e141656bc4a29a
hanrattyjen <hanratty.jen@gmail.com>
2016-11-30 12:52:53 +0000

    Added test to ensure the Sulfuras does not lose quality values.

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -4,11 +4,23 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+  subject(:gilded_rose) { described_class.new(item) }
+  let(:item) { Item.new("Item", 1, 5) }
+  let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 10, 10) }
+
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [item]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'Item'
+    end
+
+    describe 'Sulfuras' do
+      it 'does not lose it\s Sellin value or Quality' do
+        items = [sulfuras]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 10
+      end
     end
   end
 
--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -2,7 +2,7 @@ require 'item'
 
 describe Item do
 
-  subject(:account) { described_class.new("Item", 1, 5)}
+  subject(:item) { described_class.new("Item", 1, 5)}
 
   it 'is initialized with a name, value and quality' do
     expect(subject.to_s).to eq "Item, 1, 5"
